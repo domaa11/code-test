4949f2e4806507280d25204eac5ff965bc4a199b
Domagoj Filipovic <filipovic.domagoj@gmail.com>
2016-10-03 13:23:16 +1100

    Working page

--- main.go
+++ main.go
@@ -1,35 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	slice := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	theMap := map[int]string{
-		1: "Dom",
-		2: "Tina",
-		3: "Mia",
-		4: "Christian",
-		5: "Luka",
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
 	}
 
-	fmt.Println(theMap)
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.LoadHTMLGlob("templates/*.tmpl.html")
+	router.Static("/static", "static")
 
-	fmt.Println("slice:", slice)
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+	})
 
-	sliceOfSlice25 := slice[2:5]
-	sliceOfSlice47 := slice[4:7]
+	router.Run(":" + port)
 
-	fmt.Println("sliceOfSlice25:", sliceOfSlice25)
-	fmt.Println("sliceOfSlice47:", sliceOfSlice47)
-
-	sliceCopy := make([]int, 3)
-	copy(sliceCopy, slice[3:6])
-
-	slice[4] = 11
-
-	fmt.Println("slice:", slice)
-	fmt.Println("sliceOfSlice25:", sliceOfSlice25)
-	fmt.Println("sliceOfSlice47:", sliceOfSlice47)
-	fmt.Println("sliceCopy:", sliceCopy)
-
+	// slice := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	// theMap := map[int]string{
+	// 	1: "Dom",
+	// 	2: "Tina",
+	// 	3: "Mia",
+	// 	4: "Christian",
+	// 	5: "Luka",
+	// }
+	//
+	// fmt.Println(theMap)
+	//
+	// fmt.Println("slice:", slice)
+	//
+	// sliceOfSlice25 := slice[2:5]
+	// sliceOfSlice47 := slice[4:7]
+	//
+	// fmt.Println("sliceOfSlice25:", sliceOfSlice25)
+	// fmt.Println("sliceOfSlice47:", sliceOfSlice47)
+	//
+	// sliceCopy := make([]int, 3)
+	// copy(sliceCopy, slice[3:6])
+	//
+	// slice[4] = 11
+	//
+	// fmt.Println("slice:", slice)
+	// fmt.Println("sliceOfSlice25:", sliceOfSlice25)
+	// fmt.Println("sliceOfSlice47:", sliceOfSlice47)
+	// fmt.Println("sliceCopy:", sliceCopy)
+	//
 }
